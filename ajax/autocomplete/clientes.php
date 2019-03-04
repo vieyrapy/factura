64bd07c6dd5564b533b9fe905c9d930a13522e2a
vieyra <latinotanato@gmail.com>
2019-03-04 20:03:17 -0300

    Autorrellenado de ruc en cabecera de factura

--- ajax/autocomplete/clientes.php
+++ ajax/autocomplete/clientes.php
@@ -14,8 +14,8 @@ if ($con)
 		$id_cliente=$row['id_cliente'];
 		$row_array['value'] = $row['nombre_cliente'];
 		$row_array['id_cliente']=$id_cliente;
-		$row_array['ruc_cliente']=$row['ruc_cliente'];
 		$row_array['nombre_cliente']=$row['nombre_cliente'];
+		$row_array['ruc_cliente']=$row['ruc_cliente'];
 		$row_array['telefono_cliente']=$row['telefono_cliente'];
 		$row_array['email_cliente']=$row['email_cliente'];
 		array_push($return_arr,$row_array);
