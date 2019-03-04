164ab0310a3aa65c506c8f2e0cc1a2955484b459
vieyra <latinotanato@gmail.com>
2019-03-04 13:17:53 -0300

    autocomplertado cliente

--- ajax/autocomplete/clientes.php
+++ ajax/autocomplete/clientes.php
@@ -9,11 +9,12 @@ if ($con)
 	
 	$fetch = mysqli_query($con,"SELECT * FROM clientes where nombre_cliente like '%" . mysqli_real_escape_string($con,($_GET['term'])) . "%' LIMIT 0 ,50"); 
 	
-	/* Retrieve and store in array the results of the query.*/
+	/* Recupera y almacena en matriz los resultados de la consulta. */
 	while ($row = mysqli_fetch_array($fetch)) {
 		$id_cliente=$row['id_cliente'];
 		$row_array['value'] = $row['nombre_cliente'];
 		$row_array['id_cliente']=$id_cliente;
+		$row_array['ruc_cliente']=$row['ruc_cliente'];
 		$row_array['nombre_cliente']=$row['nombre_cliente'];
 		$row_array['telefono_cliente']=$row['telefono_cliente'];
 		$row_array['email_cliente']=$row['email_cliente'];
